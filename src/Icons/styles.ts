5d9433c6d4fbf2c1061db5c5fad568b933c86a86
Jonathan van Wersch <jonathanvwersch@gmail.com>
2021-02-15 19:01:48 +0100

    Second commit

--- src/Icons/styles.ts
+++
@@ -1,7 +0,0 @@
-import { createUseStyles } from "react-jss";
-import { ThemeType } from "../theme";
-
-export const useStyles = createUseStyles((theme: ThemeType) => ({
-  colour: theme.colours.offBlack,
-  size: theme.icons.size,
-}));
