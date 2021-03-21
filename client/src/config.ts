7454ed8b4cfbb6d720dd86907233bb2f00cba6c5
Juan Quintero <juan.dekked@gmail.com>
2021-03-21 13:35:15 -0500

    Deployment changes

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  api: "http://localhost:5000",
+  api: "https://dekked-api.onrender.com",
   authToken:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbF9hZGRyZXNzIjoiam9uYXRoYW52d2Vyc2NoQGdtYWlsLmNvbSIsImlhdCI6MTYxNjMyMDU4MCwiZXhwIjoxNjI2MzIwNTgwfQ.ficUWX4PihRjjYC-Rm83lwIUEypGBLXfgUGRIebY6-A",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbF9hZGRyZXNzIjoianBxbzk1MDhAZ21haWwuY29tIiwiaWF0IjoxNjE2MzUxNjA3LCJleHAiOjE2MjYzNTE2MDd9.vGTuVKGJL6sM-NuIYTxc3ToCCdvxc24a6BX4VFODp9Q",
 };
